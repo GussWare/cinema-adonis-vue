f379ca13653b3c8d94b31cc9628ecb051776f715
GussWare <gussware@gmail.com>
2020-12-24 17:05:55 -0600

    se crearon las rutas para la apis

--- cinema-adonis-backend/start/routes.js
+++ cinema-adonis-backend/start/routes.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,28 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+const Route = use("Route");
 
-Route.on('/').render('welcome')
+Route.on("/").render("welcome");
+
+Route.group(() => {
+  Route.post("login", "AuthController.login");
+  Route.post("register", "AuthController.register");
+  Route.put("profile", "AuthController.profile").middleware(["auth:jwt"]);
+
+  Route.get("cinemas", "CinemaController.findAll");
+  Route.get("cinemas/:id", "CinemaController.findById");
+
+  Route.get("genres", "GenresController.findAll");
+  Route.get("movies/:cinemaId/by-cinema", "MoviesController.findMovieByCinema");
+  Route.get("movies/:id", "MoviesController.findById");
+
+  Route.post("bookings", "BookingsController.save").middleware(["auth:jwt"]);
+  Route.get("bookings/last", "BookingsController.findByLast").middleware([
+    "auth:jwt",
+  ]);
+  Route.get(
+    "bookings/my-bookings",
+    "BookingsController.findMyBookings"
+  ).middleware(["auth:jwt"]);
+}).prefix("api/v1");
